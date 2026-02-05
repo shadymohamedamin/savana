aa39724856a4d237dc9c244cdb3850b6126c31c3
shadymohamedamin <60430920+shadymohamedamin@users.noreply.github.com>
2026-02-05 17:16:44 +0400

    add

--- resources/views/projects/partials/project-actions.blade.php
+++ resources/views/projects/partials/project-actions.blade.php
@@ -38,40 +38,5 @@
 
    
 
-    <!-- {{-- Dropdown للإضافات --}}
-    <div class="dropdown">
-        <button class="btn btn-sm dropdown-toggle"
-                style="background:#2f3a1f;color:#d4af37;"
-                data-bs-toggle="dropdown">
-            <i class="fas fa-cog"></i>
-        </button>
-
-        <ul class="dropdown-menu dropdown-menu-end" style="background:#f5f5dc;">
-            <li class="dropdown-header text-muted">
-                {{ __('مراحل المشروع') }}
-            </li>
-
-            <li>
-                <a class="dropdown-item"
-                   href="{{ url('users/'.$project->id.'/attachments/create?type=projects') }}">
-                    <i class="fas fa-folder-open me-1"></i> {{ __('عقود الاستشاري') }}
-                </a>
-            </li>
-
-            <li>
-                <a class="dropdown-item"
-                   href="{{ route('users.edit', $project->owner_id) }}">
-                    <i class="fas fa-user me-1"></i> {{ __('المالك') }}
-                </a>
-            </li>
-
-            <li>
-                <a class="dropdown-item"
-                   href="{{ route('users.edit', $project->contractor_id) }}">
-                    <i class="fas fa-hard-hat me-1"></i> {{ __('المقاول') }}
-                </a>
-            </li>
-        </ul>
-    </div> -->
 
 </div>
