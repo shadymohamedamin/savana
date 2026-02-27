4de91703eb1da165ecb09e572ad41b9d8e0f8707
shadymohamedamin <60430920+shadymohamedamin@users.noreply.github.com>
2026-02-27 15:12:43 +0400

    add

--- resources/views/projects/edit.blade.php
+++ resources/views/projects/edit.blade.php
@@ -233,7 +233,7 @@
 
 
                 <div class="flex-grow-1" style="min-width:250px;max-width:250px;">
-                    {!! Form::label('project_bank_support', __(تمويل البنك)) !!}
+                    {!! Form::label('project_bank_support', __('تمويل البنك '))!!}
                     {!! Form::number('project_bank_support', null, [
                         'class' => 'form-control rounded',
                         'min' => 800000,
