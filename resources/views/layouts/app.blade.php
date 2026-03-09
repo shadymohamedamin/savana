62c34d43f9725ea75cdfc947627de01e9f5d0f66
shadymohamedamin <60430920+shadymohamedamin@users.noreply.github.com>
2026-03-09 07:30:18 +0400

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1030,7 +1030,7 @@
 
         <span class="text-mute" style="color:#d4af37; font-weight:600;">
             {{ $endDate->format('d M Y')  }} ---
-        </span>
+        </span>      
 
         
 
