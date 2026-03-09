71aa7659d157f4b8eec3919238612e35c8c619c1
shadymohamedamin <60430920+shadymohamedamin@users.noreply.github.com>
2026-03-09 07:22:08 +0400

    add

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
         host: '0.0.0.0',
         port: 5173,
         hmr: {
-            host: '192.168.0.10',
+            host: '192.168.0.10', //'192.168.0.10',
         }
         //host: true, // exposes on network
     },
