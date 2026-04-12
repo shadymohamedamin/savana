eaf235ed785d7cdb5f6d8f76b59ec7462d8d84b5
shady <shady@savana.eng>
2026-04-12 13:49:06 +0400

    add

--- resources/views/pdf/contract_site_delivery.blade.php
+++ resources/views/pdf/contract_site_delivery.blade.php
@@ -96,24 +96,22 @@ $dayName = $today->translatedFormat('l');
 $dateFormatted = $today->translatedFormat('d/m/Y');
 @endphp -->
 
-
 @php
-use Carbon\Carbon;
 
-Carbon::setLocale('ar');
+\Carbon\Carbon::setLocale('ar');
 
-// لو التاريخ موجود
-$contractDate = $project->contract_signed_at 
-    ? Carbon::parse($project->contract_signed_at) 
+$contractDate = $project->contract_signed_at
+    ? \Carbon\Carbon::parse($project->contract_signed_at)
     : null;
 
-$dayName = $contractDate 
-    ? $contractDate->translatedFormat('l') 
+$dayName = $contractDate
+    ? $contractDate->translatedFormat('l')
     : '-';
 
-$dateFormatted = $contractDate 
-    ? $contractDate->translatedFormat('d/m/Y') 
+$dateFormatted = $contractDate
+    ? $contractDate->translatedFormat('d/m/Y')
     : '-';
+
 @endphp
 <!-- ===== Title ===== -->
 <!-- <table>
