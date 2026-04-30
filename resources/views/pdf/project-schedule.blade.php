86796157e3233287518d30bd004f38e50b56c66d
shady <shady@savana.eng>
2026-05-01 00:54:59 +0400

    add

--- resources/views/pdf/project-schedule.blade.php
+++ resources/views/pdf/project-schedule.blade.php
@@ -291,6 +291,28 @@ Carbon::setLocale('ar');
 <!-- ================= TABLE ================= -->
 <table>
 
+
+
+
+
+
+
+
+
+
+
+
+<colgroup>
+    <col style="width:5%">   <!-- رقم -->
+    <col style="width:35%">  <!-- بيان الأعمال (كبرناه) -->
+    <col style="width:8%">   <!-- النسبة المحددة (صغرناها) -->
+    <col style="width:8%">   <!-- نسبة الدفعة -->
+    <col style="width:8%">   <!-- المنجز -->
+    <col style="width:12%">  <!-- تاريخ -->
+    <col style="width:10%">  <!-- مدة -->
+    <col style="width:14%">  <!-- مبلغ -->
+</colgroup>
+
     <thead>
         <tr>
             <th>#</th>
