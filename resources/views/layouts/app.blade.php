3e88a667d1fae42500c3f260eb10ccaa4d611d19
shady <shady@savana.eng>
2026-05-04 16:44:10 +0400

    add

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1466,11 +1466,11 @@ body, html {
             <span>المخططات المعتمدة</span>
         </a>
 
-        <!-- <a href="{{ route('projects.messages.index', $project->id) }}" 
+         <a href="{{ route('projects.messages.index', $project->id) }}" 
         class="project-card {{ Route::currentRouteName() == 'projects.messages.index' ? 'active' : '' }}">
             <i class="fas fa-envelope fa-2x"></i>
             <span>الرسائل والتنبيهات</span>
-        </a> -->
+        </a> 
     @endif
 
     @if(isset($project) && in_array(auth()->user()->role_id, [1, 4, 11, 12]))
