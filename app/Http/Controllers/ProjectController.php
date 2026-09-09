d8b9a69c7d7708f83df587c9f1bb7ace22f81393
shadymohamedamin <60430920+shadymohamedamin@users.noreply.github.com>
2026-09-09 14:16:26 +0400

    add

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -223,7 +223,11 @@ if ($request->filled('search')) {
 
 
 
-    $projects = $query->orderByDesc('created_at')->paginate(0);
+    //$projects = $query->orderByDesc('created_at')->paginate(15);
+    $projects = $query
+    ->whereRaw('1 = 0')
+    ->orderByDesc('created_at')
+    ->paginate(10);
     //dd($projects);
 
 /*if (!in_array(auth()->user()->role_id, $allowedRoles)) {
